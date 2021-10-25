1cb94dedc53dffaa9cfe0abaea6a0700ebdfd711
choiseoyung <83705460+choiseoyung@users.noreply.github.com>
2021-10-25 12:39:06 +0900

    Add files via upload

---
+++ 2.연습문제.py
@@ -0,0 +1,39 @@
+# name = input("이름을 입력하시오:")
+# age = int(input("나이를 입력하시오:"))
+# year = 2021-age+100
+# print(name + "씨는" + str(year) + "년에 100살이시네요!")
+
+# n1 = int(input("첫 번째 숫자를 입력하시오:"))
+# n2 = int(input("두 번째 숫자를 입력하시오:"))
+# n3 = int(input("세 번째 숫자를 입력하시오:"))
+
+# average = (n1 + n2 + n3) / 3
+
+# print(n1, n2, n3,"의 평균은", average, "입니다.")
+
+# radius = int(input("반지름을 입력하시오:"))
+# area = 3.141592 * radius * radius
+# print("반지름이", radius, "인 원의 넓이 =", area, "입니다.")
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# radius = 50
+# t.up()
+# t.goto(0,0)
+# t.down()
+# t.circle(radius)
+
+# radius = radius + 20
+# t.up()
+# t.goto(100, 0)
+# t.down()
+# t.circle(radius)
+
+# radius = radius + 20
+# t.up()
+# t.goto(200, 0)
+# t.down()
+# t.circle(radius)
+# t._screen.exitonclick()   #화면을 마우스로 클릭해야 종료되게 하는 부분
