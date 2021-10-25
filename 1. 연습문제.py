8186cfa8d876811c62e6ddb2f5787bdecb354166
choiseoyung <83705460+choiseoyung@users.noreply.github.com>
2021-10-25 12:38:43 +0900

    Add files via upload

---
+++ 1. 연습문제.py
@@ -0,0 +1,71 @@
+# print("환영합니다.")
+# print("파이썬의 세계에 오신 것을 환영합니다.")
+# print("파이썬은 강력합니다.")
+
+# print("반갑습니다. 파이썬!")
+# print(2*3/10)
+# print("Heello", "World", "!!!")
+
+# print(7*24)
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.forward(100);
+# t.left(90)
+# t.forward(100);
+# t.right(90)
+# t.forward(100);
+# t.right(90)
+# t.forward(100);
+# t.left(90)
+# t.forward(100);
+# exitonclick()     #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.width(10)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t._screen.exitonclick()  #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.color("blue")
+# t.forward(100)
+# t._screen.exitonclick()   #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.shape("square")
+# t.forward(100)
+# t._screen.exitonclick()     #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.up()
+# t.goto(0, 0)
+# t.down()
+# t.forward(100);
+
+# t.up()
+# t.goto(0, 100)
+# t.down()
+# t.forward(100);
+# t._screen.exitonclick()   #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# t.up(); t.goto(-150, 0); t.down(); t.circle(80)
+# t.up(); t.goto(0, 0); t.down(); t.circle(80)
+# t.up(); t.goto(150, 0); t.down(); t.circle(80)
+# t.up(); t.goto(-80, -100); t.down(); t.circle(80)
+# t.up(); t.goto(80, -100); t.down(); t.circle(80)
+# t._screen.exitonclick()   #화면을 마우스로 클릭해야 종료되게 하는 부분
