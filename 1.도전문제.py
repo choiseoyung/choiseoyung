23256702ebb6e77e3c3f73a6eab8aa0641ea9309
choiseoyung <83705460+choiseoyung@users.noreply.github.com>
2021-10-27 21:44:46 +0900

    Add files via upload

---
+++ 1.도전문제.py
@@ -0,0 +1,45 @@
+# print("안녕하세요?")
+# print("programming에 입문하신 것을 축하드립니다.")
+# print(3.141592*10.0*10.0)
+# print((1/100)*1234)
+
+# import turtle             #turtle 모듈을 불러온다.
+# t = turtle.Turtle()       #turtle을 t로 선언 import turtle as t 할 수 있다.
+# t.shape("turtle");        #turtle모듈의 아이콘을 거북이 모양으로 선언
+                            #기본 아이콘은 classic, 삼각형은 triangle, 원은 circle
+# t.forward(100);           #100픽셀만큼 직진(이동 길이 즉, 각 변의 길이)
+# t.left(120);              #120도 회전(회전각도 즉, 외각이 120도를 이뤄야 정삼각형 내각 60도가 만들어진다.)
+# t.forward(100);           #100픽셀만큼 직진
+# t.left(120);              #120도 회전
+# t.forward(100);           #100픽셀만큼 직진
+# t.left(120);              #120도 회전
+
+# import turtle               
+# t = turtle.Turtle()
+# t.shape("turtle");
+
+# t.forward(100);             #100픽셀만큼 직진 (이동 길이 = 각 변의 길이)
+# t.left(90);                 #90도 회전
+# t.forward(100);             #100픽셀만큼 직진
+# t.left(90);                 #90도 회전
+# t.forward(100);             #100픽셀만큼 직진
+# t.left(90);                 #90도 회전
+# t.forward(100);             #100픽셀만큼 직진
+# t.left(90);                 #90도 회전
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle");
+
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전 
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전 
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전 
+# t.forward(100);               #100픽셀만큼 직진
+# t.left(60);                   #60도 회전
---
+++ 2.도전문제.py
@@ -0,0 +1,28 @@
+# x = 7
+# y = 6
+# print(x + y)       #13
+
+# x = '7'
+# y = '6'
+# print(x + y)       #76
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# radius = 50          #변수 radius와 값을 선언
+# x = 30               #변수 x와 값을 선언
+# t.circle(radius)     #반지름이 50인 원이 그려진다.
+# t.fd(x)              #fd는 forward와 같은 명령어이다. 앞으로 30만큼 움직인다. 
+# t.circle(radius)     #반지름이 50인 원이 그려진다.
+# x = 50               #움직이는 거리를 담는 변수 x의 값을 50픽셀로 수정한다.
+# t.fd(x)              #앞으로 50만큼 움직인다.
+# t.circle(radius)     #반지름이 50인 원이 그려진다.
+
+# x = int(input("첫 번째 정수를 입력하시오: "))
+# y = int(input("두 번째 정수를 입력하시오: "))
+
+# print(x, " + ", y, " = ", x + y)   #더하기
+# print(x, " - ", y, " = ", x - y)   #빼기
+# print(x, " * ", y, " = ", x * y)   #곱하기
+# print(x, " / ", y, " = ", x / y)   #나누기
+
