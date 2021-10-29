acb50e182d43f8b09f62a476ec6bf0cc385e98ba
choiseoyung <83705460+choiseoyung@users.noreply.github.com>
2021-10-29 12:52:54 +0900

    Add files via upload

---
+++ 4.도전문제.py
@@ -0,0 +1,35 @@
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# s = turtle.textinput("", "이름을 입력하시오:")
+
+# for i in range(4):
+#     t.write("안녕하세요?" + s + "씨, 터틀 인사드립니다.")
+#     t.left(90)
+#     t.forward(100)
+
+# print("안녕하세요?")
+# name = input("이름이 어떻게 되시나요?")
+# print("만나서 반갑습니다." + name + "씨")
+# print("이름의 길이는 다음과 같군요:", end = " ")
+# print(len(name))
+# age = int(input("나이가 어떻게 되나요?"))
+# print("내년이면", str(age+1), "이 되시는군요.")
+# hobby = input("취미가 무엇인가요?")
+# print("네 저도 "+hobby+" 좋아합니다.")
+
+# year = input("오늘의 연도를 입력하시오: ")
+# month = input("오늘의 월을 입력하시오: ")
+# date = input("오늘의 일을 입력하시오: ")
+
+# print(month+"/", date+"/", year)
+
+# import time
+
+# now = time.time()
+# thisYear = int(1970 + now // (365*24*3600))
+
+# print("올해는 ", thisYear, "입니다.")
+# age = int(input("몇 살이신지요?"))
+# print("2050년에는 ", (age + 2050 - thisYear), "살 이시군요.")
